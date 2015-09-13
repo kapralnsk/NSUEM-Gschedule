715cf5317f5f58900ece0d26383a3de8c37a94d8
Aleksandr Kaurdakov <kapralnsk@gmail.com>
2015-09-13 19:51:08 +0600

    implented parser classes; group schedule parser class

---
+++ exercise.py
@@ -0,0 +1,4 @@
+class Exercise(object):
+    def __init__(self, week, weekday, time, type, name, room):
+        self.week, self.weekday, self.time, self.type, self.name, self.room \
+            = week, weekday, time, type, name, room
--- parsers.py
+++ parsers.py
@@ -1,33 +1,73 @@
 # -*- coding: utf-8 -*-
-__author__ = 'alexandr'
+from __future__ import unicode_literals
 import urllib2
 from bs4 import BeautifulSoup
 from settings import BASE_URL
+from exercise import Exercise
 
 
 class BaseParser(object):
-
+    """
+    base class for all parsers
+    """
     def __init__(self, url):
         self.page = urllib2.urlopen(BASE_URL + url)
         self.soup = BeautifulSoup(self.page)
 
-    def parse(self):
-        pass
-
 
 class ScheduleParser(BaseParser):
+    """
+    schedule table parser
+    """
     def parse(self):
-        pass  # и тут общая логика, типа поскольку они парсят таблицу
+        """
+        parses schedule table
+        """
+        exercises = []
+        table = self.soup.find("table", {"class": "table table-hover table-bordered table-condensed"})
+        table_soup = BeautifulSoup(unicode(table), 'html5lib')
+        rows = filter(lambda item: bool(item.contents), table_soup.tbody.contents)
+        for row in rows:
+            cells = row.contents
+            for index, cell in enumerate(cells):
+                if cell.attrs.get('class'):
+                    if 'day-header' in cell.attrs.get('class'):
+                        weekday = cell.attrs.get('id')[-1]
+                        continue
+                if cell.attrs.get('id'):
+                    if 'time' in cell.attrs.get('id'):
+                        time = cell.contents[0].text
+                        continue
+                if cell.b:
+                    exercise_type = 'C'
+                else:
+                    exercise_type = 'Л'
+                if cell.select('.mainScheduleInfo'):
+                    exercise_info = cell.select('.mainScheduleInfo')[0].text.split(', ')
+                    exercise = Exercise(week=index-1, weekday=weekday, time=time,
+                                        type=exercise_type, name=exercise_info[0], room=exercise_info[1])
+                    exercises.append(exercise)
+        return exercises
 
 
 class GroupsParser(BaseParser):
+    """
+    groups list parser
+    """
     def parse(self):
         pass
 
 
 class GroupScheduleParser(ScheduleParser):
-    pass
+    """
+    parser for specific group schedule
+    """
+    def __init__(self, group):
+        super(GroupScheduleParser, self).__init__(u'group/' + group)
 
 
 class TeacherScheduleParser(ScheduleParser):
+    """
+    parser for specific teacher schedule
+    """
     pass
